dfba7ba5f2521c3e7633099baf0281809eb51717
Jim Flores <jimflores5@gmail.com>
2022-05-05 22:17:50 -0500

    Added comments to summarize possible operator positions.

--- main.py
+++ main.py
@@ -43,13 +43,30 @@ def sort_ops(operations):
 
     return ops_order.copy()
     
+def build_equation(operators, digits):
+    # No operator can go in position 1 or 8 (index 0 or 7).
+    # The '=' sign must be to the right of all other operators.
+    # 2 operators cannot be next to each other in the string.
+    # The operators list is already sorted into the proper order.
+    # For 3 operators (counting '='), acceptable postions include
+    # (2, 4, 6), (2, 4, 7), (2, 5, 7), (3, 5, 7).
+    # Index values (1, 3, 5), (1, 3, 6), (1, 4, 6), (2, 4, 6).
+    # For 2 operators, acceptable positions include
+    # (2, 6), (3, 6), (4, 6), (4, 7), (2, 5), (3, 5).
+    # Index values (1, 5), (2, 5), (3, 5), (3, 6), (1, 4), (2, 4).
+
+    pass
 
 def main():
     raw_str = '13+/958='
     correct_ans = '18 / 9 + 3 = 5'
+    known_spot = (0, '1')
+    other_str = '=5+89/34'
+    other_ans = '45 / 9 + 3 = 8'
+    other_known = (1, '5')
     
     operations, digits = parse_raw_data(raw_str)
-    print(evaluate_equation(correct_ans))
+    print(operations, digits)
 
 if __name__ == '__main__':
     main()
