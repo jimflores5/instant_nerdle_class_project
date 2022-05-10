24c281dde9fce090e8bc14e1dff5484f5576db90
Jim Flores <jflores@visitationacademy.org>
2022-05-10 14:00:54 -0500

    Completed rough algorithm for adding operations and digits to an equation template.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import math
+import random
+import copy
+
 def evaluate_equation(equa_str):
     parts = equa_str.split(' ')
     for index in range(len(parts)):
@@ -8,7 +12,7 @@ def evaluate_equation(equa_str):
         if parts[1] == '*':
             parts[0:3] = [parts[0] * parts[2]]
         elif parts[1] == '/':
-            parts[0:3] = [parts[0] // parts[2]]
+            parts[0:3] = [parts[0] / parts[2]]
         elif parts[1] == '+':
             parts[0:3] = [parts[0] + parts[2]]
         elif parts[1] == '-':
@@ -43,7 +47,7 @@ def sort_ops(operations):
 
     return ops_order.copy()
     
-def build_equation(operators, digits):
+def build_equation(template, digits):
     # No operator can go in position 1 or 8 (index 0 or 7).
     # The '=' sign must be to the right of all other operators.
     # 2 operators cannot be next to each other in the string.
@@ -54,8 +58,66 @@ def build_equation(operators, digits):
     # For 2 operators, acceptable positions include
     # (2, 6), (3, 6), (4, 6), (4, 7), (2, 5), (3, 5).
     # Index values (1, 5), (2, 5), (3, 5), (3, 6), (1, 4), (2, 4).
+    equation = ''
+    for char in template:
+        if char == 'X':
+            equation += str(digits[0])
+            digits.pop(0)
+        else:
+            equation += char
+    
+    for char in equation:
+        if char in '+-*/=':
+            equation = equation.replace(char, ' ' + char + ' ')
+    return equation
 
-    pass
+def place_ops(formats, ops, known):
+    for format in formats:
+        equation = ''
+        op_index = 0
+        for index in range(8):
+            if index == known[0]:
+                equation += known[1]
+            elif index in format:
+                equation += ops[op_index]
+                op_index += 1
+            else:
+                equation += 'X'
+        formats[format] = equation
+    return formats.copy()
+
+def build_op_dict(ops, known):
+    formats_3_ops = [(1, 3, 5), (1, 3, 6), (1, 4, 6), (2, 4, 6)]
+    formats_2_ops = [(1, 5), (2, 5), (3, 5), (3, 6), (1, 4), (2, 4)]
+    op_options = {}
+    if known[1] not in '+-*/=':
+        nix_this_position = known[0]
+    else:
+        nix_this_position = 0
+    if len(ops) == 2:
+        for option in formats_2_ops:
+            if nix_this_position not in option:
+                op_options[option] = ''
+    else:
+        for option in formats_3_ops:
+            if nix_this_position not in option:
+                op_options[option] = ''
+    return op_options.copy()
+
+def make_digit_orders(digits, known):
+    orders = []
+    for digit in digits:
+        if str(digit) == known[1]:
+            digits.remove(digit)
+    num_digits = len(digits)
+    num_permutations = math.factorial(num_digits)
+    while len(orders) < num_permutations:
+        temp_list = digits.copy()
+        random.shuffle(temp_list)
+        if temp_list not in orders:
+            orders.append(temp_list.copy())
+    orders.sort()
+    return copy.deepcopy(orders)
 
 def main():
     raw_str = '13+/958='
@@ -64,9 +126,29 @@ def main():
     other_str = '=5+89/34'
     other_ans = '45 / 9 + 3 = 8'
     other_known = (1, '5')
-    
+
+    operations, digits = parse_raw_data(other_str)
+    options = build_op_dict(operations, other_known)
+    equations = place_ops(options.copy(), operations.copy(), other_known)
+    orders = make_digit_orders(digits, other_known)
+    solution = ''
+    for equation in equations.values():
+        for order in orders:
+            temp = build_equation(equation, order.copy())
+            if evaluate_equation(temp):
+                solution = temp
+    print(f"{other_str} ---> {solution}")
+    print('---')
     operations, digits = parse_raw_data(raw_str)
-    print(operations, digits)
+    options = build_op_dict(operations, known_spot)
+    equations = place_ops(options.copy(), operations.copy(), known_spot)
+    orders = make_digit_orders(digits, known_spot)
+    for equation in equations.values():
+        for order in orders:
+            temp = build_equation(equation, order.copy())
+            if evaluate_equation(temp):
+                solution = temp
+    print(f"{raw_str} ---> {solution}")
 
 if __name__ == '__main__':
     main()
